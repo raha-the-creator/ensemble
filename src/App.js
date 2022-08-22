349696f28ff81f2cc59b8c3f95c422ee84ee09a7
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-08-21 20:57:09 -0700

    save and delete from local storage

--- src/App.js
+++ src/App.js
@@ -29,17 +29,32 @@ function App() {
     getMovieRequest(searchValue)
   }, [searchValue]) // every time serach value changes call getMovieRequest
 
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+      localStorage.getItem('movie-favourites')
+    )
+
+    if (movieFavourites) {
+      setFavourites(movieFavourites)
+    }
+  }, [])
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('movie-favourites', JSON.stringify(items))
+  };
+
   const addFavouriteMovie = (movie) => {
     const newFavouriteList = [...favourites, movie]
     setFavourites(newFavouriteList)
+    saveToLocalStorage(newFavouriteList)
   }
 
   const removeFavouriteMovie = (movie) => {
     const newFavouriteList = favourites.filter(
-      (favourite) => favourite.imdID !== movie.imdbID
+      (favourite) => favourite.imdbID !== movie.imdbID
     )
-
     setFavourites(newFavouriteList)
+    saveToLocalStorage(newFavouriteList)
   }
 
   return (
